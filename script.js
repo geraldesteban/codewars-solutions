748b696221f4e643a947d60ca03c82683f0bc145
geraldesteban <wakin.esteban@gmail.com>
2024-01-03 20:31:52 +0800

    add solution

--- script.js
+++ script.js
@@ -226,3 +226,15 @@ function simpleMultiplication(number) {
 }
 console.log(simpleMultiplication(8));
  */
+
+/* 
+// Count the Digit
+function nbDig(n, d) {
+  let result = "";
+  for (let i = 0; i <= n; i++) {
+    result += i ** 2;
+  }
+  return result.split(d).length - 1;
+}
+console.log(nbDig(10, 1));
+ */
