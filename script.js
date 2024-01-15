cffff32e668d1d2af35af91650f2791b6143958a
geraldesteban <wakin.esteban@gmail.com>
2024-01-15 10:37:30 +0800

    add solution

--- script.js
+++ script.js
@@ -312,3 +312,19 @@ function positiveSum(arr) {
 }
 console.log(positiveSum([1, -4, 7, 12]));
  */
+
+// Count of positives / sum of negatives
+function countPositivesSumNegatives(input) {
+  return !input || input.length === 0
+    ? []
+    : [
+        input.filter(p => p > 0).length,
+        input.filter(n => n < 0).reduce((acc, cur) => acc + cur, 0),
+      ];
+}
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ])
+);
+console.log(countPositivesSumNegatives([]));
